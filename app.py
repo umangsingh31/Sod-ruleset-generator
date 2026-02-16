c73d7e3328d02f892a1006bae87e9b3a1d93ae60
umangsingh31 <Umang.Singh@secarmor.in>
2026-02-16 10:30:06 +0530

    Added dockerfile and xls conersion logic

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -62,4 +63,71 @@ async def generate(
         output_path,
         filename="output.xlsx",
         media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+'''
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi.staticfiles import StaticFiles
+import shutil
+import os
+import uuid
+
+from generator import generate_sre
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/generate")
+async def generate(
+    template: UploadFile = File(...),
+    projects: UploadFile = File(...),
+    owners: UploadFile = File(...),
+    baseline: UploadFile = File(None),
+):
+    workdir = f"work_{uuid.uuid4().hex}"
+    os.makedirs(workdir, exist_ok=True)
+
+    template_path = os.path.join(workdir, "template.xlsx")
+    projects_path = os.path.join(workdir, "projects.xlsx")
+    owners_path = os.path.join(workdir, "owners.xlsx")
+    output_path = os.path.join(workdir, "output.xlsx")
+
+    baseline_path = None
+
+    # Save uploaded files
+    with open(template_path, "wb") as f:
+        shutil.copyfileobj(template.file, f)
+
+    with open(projects_path, "wb") as f:
+        shutil.copyfileobj(projects.file, f)
+
+    with open(owners_path, "wb") as f:
+        shutil.copyfileobj(owners.file, f)
+
+    if baseline:
+        baseline_path = os.path.join(workdir, "baseline.xlsx")
+        with open(baseline_path, "wb") as f:
+            shutil.copyfileobj(baseline.file, f)
+
+    # Run generator
+    xls_path = generate_sre(
+    template_path=template_path,
+    projects_path=projects_path,
+    owners_path=owners_path,
+    baseline_path=baseline_path,
+    output_path=output_path
+    )
+
+    return FileResponse(
+        xls_path,
+        filename="output.xls",
+        media_type="application/vnd.ms-excel"
     )
